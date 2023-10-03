3a82af88e3474a79629cda65394c1ec7ecee633b
wjsoj <wjs@wjsphy.top>
2023-10-03 13:03:05 +0800

    new submit page

--- src/components/submitLink.vue
+++ src/components/submitLink.vue
@@ -5,6 +5,7 @@ import { request } from '@/utils/request'
 import { UploadFilled } from '@element-plus/icons-vue'
 
 let link = ref('')
+let isPosting = ref(false)
 
 onMounted(() => {
   document.querySelector('footer').style.position = 'fixed'
@@ -28,12 +29,36 @@ async function submitLink() {
     })
     return
   }
-  await request({
-    url: '/user/submit/link',
-    method: 'post',
-    data: {
-      link: link.value
+  isPosting.value = true
+  await request.post(
+    "/user/submit/link",
+    link.value,
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
+  ).then(res => {
+    isPosting.value = false
+    if (res.data.code === 1) {
+      ElMessage({
+        message: '提交成功',
+        type: 'success'
+      })
+      link.value = ''
+    } else {
+      ElMessage({
+        message: res.data.msg,
+        type: 'error'
+      })
+      console.log(res.data)
+    }
+  }).catch(err => {
+    console.log(err)
+    ElMessage({
+      message: '提交失败',
+      type: 'error'
+    })
   })
 }
 
@@ -41,6 +66,8 @@ async function submitLink() {
 
 <template>
   <div class="min-h-[80vh] flex flex-col justify-center items-center px-4 py-10 md:px-32 lg:px-80">
+    <!-- 图片 -->
+    <img src="/favicon.png" alt="submit" class="w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 mb-5">
     <el-input
       v-model="link"
       placeholder="请输入要提交的链接"
@@ -48,6 +75,6 @@ async function submitLink() {
       class="mb-5"
       size="large"
     />
-    <el-button type="primary" @click="submitLink" size="large">提交</el-button>
+    <el-button type="primary" @click="submitLink" v-loading="isPosting" size="large">提交</el-button>
   </div>
 </template>
