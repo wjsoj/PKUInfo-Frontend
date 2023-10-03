828bbd3b9e686d96dd2fe752cebc35e7fb8f47b2
wjsoj <wjs@wjsphy.top>
2023-10-03 13:10:16 +0800

    new submit page

--- src/components/submitLink.vue
+++ src/components/submitLink.vue
@@ -42,7 +42,7 @@ async function submitLink() {
     isPosting.value = false
     if (res.data.code === 1) {
       ElMessage({
-        message: '提交成功',
+        message: '提交成功，请勿重复提交',
         type: 'success'
       })
       link.value = ''
@@ -68,13 +68,17 @@ async function submitLink() {
   <div class="min-h-[80vh] flex flex-col justify-center items-center px-4 py-10 md:px-32 lg:px-80">
     <!-- 图片 -->
     <img src="/favicon.png" alt="submit" class="w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 mb-5">
-    <el-input
-      v-model="link"
-      placeholder="请输入要提交的链接"
-      :prefix-icon="UploadFilled"
-      class="mb-5"
-      size="large"
-    />
-    <el-button type="primary" @click="submitLink" v-loading="isPosting" size="large">提交</el-button>
+    <div class="h-10 md:h-12 lg:h-14 w-full my-4">
+      <el-input
+        v-model="link"
+        placeholder="请输入要提交的链接"
+        :prefix-icon="UploadFilled"
+        class="mb-5 h-full w-full"
+        size="large"
+      />
+    </div>
+    <div class="flex w-full justify-center">
+      <el-button type="primary" @click="submitLink" v-loading="isPosting" size="large" class="w-1/4 md:w-1/5 xl:w-1/6">提交</el-button>
+    </div>
   </div>
 </template>
