1f6b31c95fec5f8d4013ad517198889ae42e5a1e
wjsoj <wjs@wjsphy.top>
2023-10-05 09:50:10 +0800

    improve the performance of calendar

--- src/components/calendarCell.vue
+++ src/components/calendarCell.vue
@@ -19,41 +19,43 @@ let { activities, day } = defineProps({
       {{ day.split('-')[2]}}
     </h1>
     <div v-if="activities[0]==='不在支持的日期范围内'">
-      不在支持的日期范围内
+      <br/>
     </div>
     <div v-else>
       <div v-if="activities.length">
+
         {{ activities[0].title }} 
         <br />
         等{{ activities.length }}个活动
+
+        <el-drawer
+          v-model="drawer"
+          :title="day+'活动列表'"
+          size="70%"
+        >
+          <el-collapse accordion>
+            <el-timeline>
+              <el-timeline-item
+                v-for="activity in activities"
+                :key="activity.id"
+                :timestamp="activity.startDate+' '+activity.startTime+'-'+activity.endTime"
+              >
+                <el-collapse-item :title="activity.title" :name="activity.id">
+                  <!-- 将activity所有字段以key - value的表格形式显示在一起 -->
+                  <div v-for="(value, key) in activity" :key="key" class="grid grid-cols-3">
+                    <p class="font-semibold">{{ key }}：</p>
+                    <p class=" col-span-2">{{ value }}</p>
+                  </div>
+                </el-collapse-item>
+              </el-timeline-item>
+            </el-timeline>
+          </el-collapse>
+        </el-drawer>
+
       </div>
       <div v-else>
         暂无活动
       </div>
     </div>
   </div>
-
-  <el-drawer
-    v-model="drawer"
-    :title="day+'活动列表'"
-    size="70%"
-  >
-    <el-collapse accordion>
-      <el-timeline>
-        <el-timeline-item
-          v-for="activity in activities"
-          :key="activity.id"
-          :timestamp="activity.startDate+' '+activity.startTime+'-'+activity.endTime"
-        >
-          <el-collapse-item :title="activity.title" :name="activity.id">
-            <!-- 将activity所有字段以key - value的表格形式显示在一起 -->
-            <div v-for="(value, key) in activity" :key="key" class="grid grid-cols-3">
-              <p class="font-semibold">{{ key }}：</p>
-              <p class=" col-span-2">{{ value }}</p>
-            </div>
-          </el-collapse-item>
-        </el-timeline-item>
-      </el-timeline>
-    </el-collapse>
-  </el-drawer>
 </template>
--- src/components/timeCalendar.vue
+++ src/components/timeCalendar.vue
@@ -2,10 +2,12 @@
 import { useStore } from '../stores'
 import { storeToRefs } from 'pinia'
 import calendarCell from './calendarCell.vue'
-import { onUpdated, onMounted } from 'vue';
+import { onUpdated, onMounted, ref, watch } from 'vue';
+import { ElMessage } from 'element-plus';
 
 const store = useStore()
 const { loadingCombine,activityList } = storeToRefs(store)
+const date = ref(new Date())
 
 function isInRange(dayString) {
   const day = new Date(dayString)
@@ -21,6 +23,20 @@ function getActivitiesByDate (date) {
     return ['不在支持的日期范围内']
   }
 }
+
+watch(date, (newDate, oldDate) => {
+  // newDate在今天之后，且在今天之后90天之内
+  if (newDate.getTime() > new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0 ,0).getTime() && newDate.getTime() < new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 90, 23, 59, 59).getTime()) {
+    return
+  }
+  // newDate在今天之前，且newDate的月底在今天之前30天之内
+  else if (newDate.getTime() < new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0 ,0).getTime() && new Date(newDate.getFullYear(), newDate.getMonth() + 1, 0, 23, 59, 59).getTime() > new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 30, 0, 0 ,0).getTime()) {
+    return
+  }
+  ElMessage.warning('不在支持的日期范围内')
+  // 还原为旧日期
+  date.value = oldDate
+})
 
 // 监听窗口内容变化，如果窗口内容高度不足窗口高度，将footer固定在底部
 onUpdated(() => {
@@ -42,9 +58,9 @@ onMounted(() => {
 
 <template>
   <div class="min-h-[80vh] mx-10 my-6">
-    <el-calendar v-loading="loadingCombine">
+    <el-calendar v-loading="loadingCombine" v-model="date">
       <template #date-cell="{ data }">
-          <calendar-cell :activities="getActivitiesByDate(data.day)" :day="data.day"/>
+        <calendar-cell :activities="getActivitiesByDate(data.day)" :day="data.day"/>
       </template>
     </el-calendar>
   </div>
