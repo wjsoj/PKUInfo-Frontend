88b66825f034893309a25115bcf6cc35dcdd228e
wjsoj <wjs@wjsphy.top>
2023-10-07 13:39:41 +0800

    improve the performance of mainTable

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -13,14 +13,20 @@ let startDate = ref(new Date(new Date().getFullYear(), new Date().getMonth(), ne
 let endDate = ref(new Date(new Date().getFullYear(), new Date().getMonth() , new Date().getDate() + 30, 23, 59, 59))
 let keyWord = ref('')
 
+let currentPage = ref(1)
+let pageSize = ref(100)
+
 const result = computed(() => activityList.value.filter(activity => {
   let activityDate = new Date(activity.startDate)
   return activityDate.getTime() >= startDate.value.getTime() && activityDate.getTime() <= endDate.value.getTime()
 }).filter(activity => {
+  // 如果关键词为空，返回所有活动
   if (keyWord.value === '') {
     return true
   }
-  return activity.title.includes(keyWord.value) || activity.description.includes(keyWord.value)
+  // 使用正则表达式快速匹配关键词
+  let reg = new RegExp(keyWord.value, 'g')
+  return reg.test(activity.title) || reg.test(activity.description)
 }).sort((a,b) => {
   return new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
 })
@@ -69,7 +75,7 @@ onUpdated(() => {
   </div>
   
   <div class="md:mx-10 lg:mx-16">
-    <el-table :data="result" :stripe="!isDark" v-loading="isLoading">
+    <el-table :data="result.slice((currentPage - 1) * pageSize, currentPage * pageSize)" :stripe="!isDark" v-loading="isLoading">
       <el-table-column type="expand" width="30">
         <template #default="{row}">
           <div class="ml-10 mr-2">
@@ -95,9 +101,14 @@ onUpdated(() => {
         </template>
       </el-table-column>
     </el-table>
+
+    <div class="flex justify-center">
+      <el-pagination layout="total, prev, pager, next, jumper" :total="result.length" :page-size="pageSize" @current-change="currentPage = $event" hide-on-single-page class="my-4 md:my-8" v-if="!isLoading">
+      </el-pagination>
+    </div>
   </div>
 
-  <h1 class="text-base text-center my-2 md:my-4" v-if="!isLoading">共{{ result.length }}条</h1>
+  <h1 class="text-base text-center my-2 md:my-4" v-if="!isLoading && result.length <= 100">共{{ result.length }}条</h1>
   <div class="md:hidden">
     <el-backtop :right="20" :bottom="70" />
   </div>
