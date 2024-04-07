2e78440b2048431878c6053c4c1315ba88933e5d
wjsoj <wjs@wjsphy.top>
2024-04-07 21:13:27 +0800

    update qrcode

--- src/utils/visit.js
+++ src/utils/visit.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const token = 'vfnaILJDkHQv3IWIqzppbDReif6l1beNXW2N6qWjZqfVi/XpQqGDni6DXVRsv0NCAyjMpA3FdfsSfez3RBhCEeeTWNluSzXvi1A/6p68Vrftno+M3Bfz6hJvumHzd5T2JQb7UzReD6Dy/UBgaXik5//j0eATBD/USBFTof9TSqaXDTxYQihsCWLI8zGOYwlWbjDh+qESBNYztkYVNL59mRVtRswy9PE0sNdlXgauq2I8tJgm8BhP1atj2sajvTdk4IeZvxBbLBaED9BB1UOnnXyA0RShCEkSEjS9D/QCHybGcUy5Thkxfens98OkJI+nC64fIU9CWKENnyfQybHh/Denj4FH072dqA=='
+const token = import.meta.env.VITE_UMAMI_TOKEN
 const websiteId = '25ac71b5-ded7-4612-8938-3a84fe76124f'
 const baseURL = 'https://pkuinfo.lcpu.dev/monitor/api'
 // 开发环境，需配置代理
